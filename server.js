34ac8a9a6d35da38f51f95cc637d97d0e558906c
Rodolfo <rodolfo.singular@gmail.com>
2025-08-07 15:34:10 -0300

    update

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const express = require("express");
 const path = require("path");
 const dialogflow = require("@google-cloud/dialogflow");
 const uuid = require("uuid");
+const { WebhookClient } = require("dialogflow-fulfillment");
 require("dotenv").config();
 
 const app = express();
 const port = 3000;
+
+// Cargar el archivo JSON que emula una base de datos
+const restaurantes = require("./restaurantes.json");
 
 // Cargar el archivo de clave
 const CREDENTIALS = require("./dialogflow-key.json");
@@ -28,6 +32,22 @@ const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
 // Middleware
 app.use(express.json());
 app.use(express.static("public")); // para servir el script JS
+
+// Funciones
+
+  function recomendarRestaurantes(tipoComida, zona) {
+    const tipo = tipoComida?.toLowerCase();
+    const lugar = zona?.toLowerCase();
+
+    const lugares = restaurantes[tipo][lugar];
+    console.log(lugares);
+
+    if (lugares && lugares.length > 0) {
+      return `Te recomiendo estos lugares de ${tipoComida} en ${zona}: ${lugares.join(", ")}.`;
+    } else {
+      return `No encontre restaurantes de ${tipoComida} en ${zona}. Querés que busque en otra zona o con otro tipo de comida?`;
+    }
+  }
 
 // Ruta para el widget (iframe)
 app.get("/widget", (req, res) => {
@@ -68,4 +88,39 @@ app.post("/api/chat", async (req, res) => {
 // Iniciar servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}/widget`);
+});
+
+app.post("/webhook", express.json(), (req, res) => {
+  const agent = new WebhookClient({ request: req, response: res });
+
+  function handleRecomendacion(agent) {
+
+    const parameters = agent.parameters;
+    const outputContexts = agent.contexts;
+
+    // Intentar recuperar parámetros desde contextos anteriores
+    const tipoComida = parameters.tipocomida?.[0] || parameters.tipocomida || '';
+    const zona = parameters.zona?.[0] || parameters.zona || '';
+
+    if (!tipoComida) {
+      const contexto = outputContexts.find(ctx => ctx.name.includes('esperando_zona'));
+      tipoComida = contexto?.parameters?.tipocomida;
+    }
+
+    if (!zona) {
+      const contexto = outputContexts.find(ctx => ctx.name.includes('esperando_comida'));
+      zona = contexto?.parameters?.zona;
+    }
+
+    console.log("Parametros recibidos:", agent.parameters);
+
+
+    const respuesta = recomendarRestaurantes(tipoComida, zona);
+    agent.add(respuesta);
+  }
+
+  let intentMap = new Map();
+  intentMap.set("RecomendarRestaurante", handleRecomendacion);
+
+  agent.handleRequest(intentMap);
 });
